c72707b202fd269873b92dd4598b5aefb602b459
b17v134 <b17v134@users.noreply.github.com>
2023-05-19 06:35:57 +0000

    Added udp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::{net::{UdpSocket, SocketAddr}, process::exit};
+use std::thread;
+
+fn handle_client(socket_addr: &SocketAddr, len: usize, buf: [u8; 4096]) {
+    println!("buf = {:?}", buf);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let socket: UdpSocket;
+    let result = UdpSocket::bind("127.0.0.1:8888");
+    match result{
+        Ok(s) => { socket = s; },
+        Err(e) =>  {
+            println!("{}", e);
+            exit(1)
+        }
+    }
+
+    loop {
+        let mut buf = [0; 4096];
+        let result = socket.recv_from(&mut buf);
+        match result {
+            Ok (s) => {
+                thread::spawn(move || handle_client(&s.1, s.0, buf));
+            },
+            Err(e) =>  {
+                println!("{}", e);
+                exit(1)
+            }
+        } 
+    }
 }
