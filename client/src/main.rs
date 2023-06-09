7c6d8f0d3ec6d39c4d7cb7e2593e63f5df5ede62
b17v134 <b17v134@users.noreply.github.com>
2023-06-09 13:23:10 +0000

    Removed OutputFormat from client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,25 +1,10 @@
 extern crate clap;
 extern crate resolve_dns;
 
-use clap::{Parser, ValueEnum};
+use clap::Parser;
 
 fn default_address() -> String {
     "127.0.0.1".to_string()
-}
-
-#[derive(ValueEnum, Clone, Debug)]
-enum OutputFormat {
-    Json,
-    Plain,
-    Yaml,
-}
-
-fn arg_output_format_as_output_format(output_format: &OutputFormat) -> &resolve_dns::OutputFormat {
-    match output_format {
-        OutputFormat::Json => &resolve_dns::OutputFormat::Json,
-        OutputFormat::Plain => &resolve_dns::OutputFormat::Plain,
-        OutputFormat::Yaml => &resolve_dns::OutputFormat::Yaml,
-    }
 }
 
 #[derive(Parser, Debug)]
@@ -40,8 +25,8 @@ struct Args {
     #[arg(short, long, default_value_t = String::from(resolve_dns::DNS_STR_CLASS_IN))]
     class: String,
 
-    #[arg(short, long, value_enum, default_value_t = OutputFormat::Plain)]
-    output_format: OutputFormat,
+    #[arg(short, long, value_enum, default_value_t = resolve_dns::OutputFormat::Plain)]
+    output_format: resolve_dns::OutputFormat,
 
     host: String,
 }
@@ -59,7 +44,7 @@ fn main() {
     };
     let result = resolve_dns::resolv(request);
     match result {
-        Ok(message) => message.print(arg_output_format_as_output_format(&args.output_format)),
+        Ok(message) => message.print(&args.output_format),
         Err(e) => println!("{e}"),
     }
 }
